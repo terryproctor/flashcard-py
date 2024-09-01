360a756627c9bcc08f715a2017a537db2530ff55
terryproctor <terryproctor2015@outlook.com>
2024-09-01 22:32:09 +0100

    solved UI alignment problem partially

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ window.title("Flashy")
 card_front = PhotoImage(file="images/card_front.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front)
-canvas.grid(row=0, column=0, columnspan=2)
+canvas.grid(row=0, column=0, columnspan=3)
 
 cross_image = PhotoImage(file="images/wrong.png")
 cross_button = Button(image = cross_image, highlightthickness = 0)
